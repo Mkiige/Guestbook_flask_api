0b1caf1a7fc88e79f38a34cf682d0d782c30fd1c
Mkiige <mkiige77@gmail.com>
2023-02-22 15:15:06 +0300

    delete  person

--- people.py
+++ people.py
@@ -3,6 +3,7 @@
 
 from datetime import datetime
 from flask import abort
+from flask import abort, make_response
 
 # Create a new person
 def create(person):
@@ -61,6 +62,18 @@ def update(lname, person):
         PEOPLE[lname]["fname"] = person.get("fname", PEOPLE[lname]["fname"])
         PEOPLE[lname]["timestamp"] = get_timestamp()
         return PEOPLE[lname]
+    else:
+        abort(
+            404,
+            f"Person with last name {lname} not found"
+        )        
+
+def delete(lname):
+    if lname in PEOPLE:
+        del PEOPLE[lname]
+        return make_response(
+            f"{lname} successfully deleted", 200
+        )
     else:
         abort(
             404,
