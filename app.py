09d7298956a4092d90d6b25b11be149def1cdb35
Mkiige <mkiige77@gmail.com>
2023-02-22 13:30:43 +0300

    Add Connexion to the App

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-# app.py
 #This is a simple Python Flask web application that defines a route for the root URL path ("/"). 
 #The route function returns the result of calling the render_template function, which renders a Jinja2 template called "home.html". 
 #The __name__ == "__main__" condition ensures that the server only runs when the script is executed directly (as opposed to when it is imported as a module).
