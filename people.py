2f55f4b2db2c359f99cfc58e2753aa44eab9bf5e
Mkiige <mkiige77@gmail.com>
2023-02-22 13:45:10 +0300

    Return Data From Your People Endpoint

---
+++ people.py
@@ -0,0 +1,28 @@
+# this code provides basic functionality for managing a list of people
+# including the ability to read all the people in the list along with their first and last names and timestamps
+
+from datetime import datetime
+
+def get_timestamp():
+    return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
+
+PEOPLE = {
+    "Fairy": {
+        "fname": "Tooth",
+        "lname": "Fairy",
+        "timestamp": get_timestamp(),
+    },
+    "Ruprecht": {
+        "fname": "Knecht",
+        "lname": "Ruprecht",
+        "timestamp": get_timestamp(),
+    },
+    "Bunny": {
+        "fname": "Easter",
+        "lname": "Bunny",
+        "timestamp": get_timestamp(),
+    }
+}
+
+def read_all():
+    return list(PEOPLE.values())
